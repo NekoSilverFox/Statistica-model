74a198db38962f796e69319343b994f32930df63
NekoSilverFox <weidufox@gmail.com>
2022-03-18 15:09:50 +0300

    基本验证无误

--- 007 - lab4/main.py
+++ 007 - lab4/main.py
@@ -13,63 +13,66 @@
     λ(t) - Интенсивность отказов            失败率，根据统计学的定义，故障率是指在相关时间段内，故障产品的数量与正在运行的产品的平均数量的比率
 
 """
-import random
+import numpy as np
 import math
+from scipy.stats import norm
 
 
 def LFRS(x):
     T = 8760
-    # return (((x[1] > T) & (x[2] > T) | (x[3] > T)) \
-    #         & ((x[4] > T) & (x[5] > T)) \
-    #         & ((x[6] > T) & (x[7] > T) | (x[8] > T) & (x[9] > T) | (x[10] > T) & (x[11] > T)))
+
     return (((x[0] > T) & (x[1] > T) | (x[2] > T)) \
             & ((x[3] > T) & (x[4] > T)) \
             & ((x[5] > T) & (x[6] > T) | (x[7] > T) & (x[8] > T) | (x[9] > T) & (x[10] > T)))
 
 
 def work(L):
-    N = 3301299
+    N = 9539
     m = 3
     lambdaF = [40 * pow(10, -6),
                10 * pow(10, -6),
                80 * pow(10, -6)]
     n = [3, 2, 6]
-    d = 0
+    num_broken = 0
 
     for k in range(0, N):
         x = []
+
         for i in range(0, m):
             t = []
 
             for j in range(0, n[i]):
-                alpha = random.random()
+                alpha = np.random.random()
                 t.append(-math.log(alpha) / lambdaF[i])
 
             for j in range(0, L[i]):
-                l = t.index(min(t))
-                t[l] = t[l] - math.log(random.random()) / lambdaF[i]
+                index_min = t.index(min(t))
+                t[index_min] = t[index_min] - math.log(np.random.random()) / lambdaF[i]
 
             for j in range(0, n[i]):
                 x.append(t[j])
 
-        if not LFRS(x):
-            d = d + 1
+        num_broken = num_broken + int(not LFRS(x))
 
-    return 1 - d / N
+    return 1 - num_broken / N
 
 
 if __name__ == '__main__':
-    p0 = 0.99
-    L = [0, 0, 0]
+    # ppf = norm.ppf(q=0.999, loc=0, scale=1)  # 标准正态分布的四分位数
 
-    for i in range(1, 4):
-        L[0] = i
-        for j in range(1, 3):
-            L[1] = j
-            for k in range(1, 7):
-                L[2] = k
-                P = work(L)
-                if (P > p0):
-                    print('\033[32mP = ', P, "  [", L, "]   ", sum(L), '+\033[0m')
+    p0 = 0.99
+    arr_block = [0, 0, 0]
+    for type_1 in range(0, 4):
+        arr_block[0] = type_1
+
+        for type_2 in range(0, 3):
+            arr_block[1] = type_2
+
+            for type_3 in range(0, 7):
+                arr_block[2] = type_3
+                p_broken = work(arr_block)
+
+                if p_broken > p0:
+                    print('\033[32mP = ', p_broken, "  ", arr_block, "   ", sum(arr_block), '+\033[0m')
                 else:
-                    print("P = ", P, "  [", L, "]   ", sum(L))
+                    print("P = ", p_broken, "  ", arr_block, "   ", sum(arr_block))
